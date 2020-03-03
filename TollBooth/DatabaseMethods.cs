0f03160cbaf51513cd64bb0b2ab27ca42fc677c2
David Cobb <demo@davidcobb.net>
2020-03-03 19:23:21 +0000

    test deploy change

--- TollBooth/DatabaseMethods.cs
+++ TollBooth/DatabaseMethods.cs
@@ -44,8 +44,6 @@ namespace TollBooth
                         new FeedOptions() { EnableCrossPartitionQuery = true, MaxItemCount = 100 })
                     .Where(l => l.exported == false)
                     .ToList();
-                // TODO 6: Remove the line below.
-                //licensePlates = new List<LicensePlateDataDocument>();
             }
 
             exportedCount = licensePlates.Count();
